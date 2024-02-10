df78468b96fdecb06b9048d4c5f5fe1ae5a04bfe
Ayibiowu Gbenga <ayibiowug@gmail.com>
2024-02-10 06:16:02 +0100

    my first commit

---
+++ assignment-week2/assignment.js
@@ -0,0 +1,8 @@
+// searchBox = document.getElementById ("search-input") ;
+// searchBox.addEventListener("click", () => {
+//     searchBox.style.borderColor 
+// })
+
+// changeColor = () => {
+//     document.getElementById("search-input").style.border=none
+// }
